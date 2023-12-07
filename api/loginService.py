585cc702e5af23f073443d38962c5a2cf4c089d2
dice1996 <d.cerjak@yahoo.com>
2023-12-07 22:36:31 +0100

    Approved

--- api/loginService.py
+++ api/loginService.py
@@ -1,5 +1,5 @@
 import requests
-from flask import request, redirect, url_for, flash, session
+from flask import request, redirect, url_for, flash, session, make_response 
 
 NOCO_DB_URL = "https://db.ceky.me/api/v1/db/data/v1/"
 API_TOKEN = "jpgFE3jk3pN9R4S52be05kEG9D-EQbBjiFNrig4x"
@@ -36,8 +36,20 @@ def authenticate():
         session['email'] = user['email']
         session['cms_user'] = user['cms_user']
         session['cms_password'] = user['cms_password']
-        flash(f"Logged in as {user['username']} with role {user['role']}", "success")
-        return redirect(url_for('index'))  # Redirect to the main page after successful login
+        
+        if 'remember-me' in request.form:
+            # Set a persistent HTTP cookie to maintain the user's logged-in state
+            resp = make_response(redirect(url_for('index')))
+            resp.set_cookie('remember-me', str(user["Id"]), max_age=86400 * 30)
+            return resp
+        else:
+            # Clear the "remember-me" cookie if it exists
+            resp = make_response(redirect(url_for('index')))
+            resp.set_cookie('remember-me', '', max_age=0)
+            return resp
+        
+        #flash(f"Logged in as {user['username']} with role {user['role']}", "success")
+        #return redirect(url_for('index'))  # Redirect to the main page after successful login
     else:
         flash("Invalid username or password", "danger")
         return redirect(url_for('login'))  # Redirect back to the login page with an error message
